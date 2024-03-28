90c63c18c6ea77ba4d91ba2870ab0ce87680e375
Somin Lee <50287759+chloe73@users.noreply.github.com>
2024-03-28 21:14:12 +0900

    Title: 포탑 부수기, Time: 229ms, Memory: 18MB, Status: Wrong Answer - CodeTree

--- 240328/포탑 부수기/destroy-the-turret.java
+++ 240328/포탑 부수기/destroy-the-turret.java
@@ -82,6 +82,9 @@ public class Main {
 		while(K-- > 0) {
 			visited = new boolean[N][M];
 			
+            // 만약 부서지지 않은 포탑이 1개가 된다면 그 즉시 중지됩니다.
+			if(aPq.size() == 1) break;
+
 			// 1. 공격자 선정
 			// 부서지지 않은 포탑 중 가장 약한 포탑이 공격자로 선정됩니다. 
 			Top attacker = aPq.poll();
